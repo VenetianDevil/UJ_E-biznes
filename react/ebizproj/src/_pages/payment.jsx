ff953e8a3c64a7a5920a07a5183123c5113e393b
VenetianDevil <rolcia07@gmail.com>
2022-06-17 18:07:31 +0200

    stripe redirect address to azure deploy

--- react/ebizproj/src/_pages/payment.jsx
+++ react/ebizproj/src/_pages/payment.jsx
@@ -85,7 +85,7 @@ const CheckoutForm = () => {
       //`Elements` instance that was used to create the Payment Element
       elements,
       confirmParams: {
-        return_url: "http://localhost:3000/cart/ordered",
+        return_url: "http://ebiz.azurewebsites.net/cart/ordered",
       },
     }).then(async () =>{
       console.log('success');
