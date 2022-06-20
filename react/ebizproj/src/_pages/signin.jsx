33f7bc33370300106357f91ec89691573139c469
VenetianDevil <rolcia07@gmail.com>
2022-06-20 16:10:29 +0200

    cosmetic change

--- react/ebizproj/src/_pages/signin.jsx
+++ react/ebizproj/src/_pages/signin.jsx
@@ -1,5 +1,4 @@
-import React from 'react';
-import { useState } from 'react';
+import React, { useState }  from 'react';
 import { useNavigate } from "react-router-dom";
 import { Button, Row, Col, Form } from 'react-bootstrap';
 import { NotificationManager } from 'react-notifications';
